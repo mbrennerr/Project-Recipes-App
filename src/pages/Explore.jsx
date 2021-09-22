043c0c24d9e52865aabd8f5263e6242e7767d8bf
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-09-22 14:23:19 -0300

    Estrutura o Footer e cria nova páginas - Requisitos 19 até 24.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 const Explore = () => (
-  <h1>Explore</h1>
+  <div>
+    <h1>Explore</h1>
+    <Footer />
+  </div>
 );
 
 export default Explore;
