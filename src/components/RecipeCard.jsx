d531eb6815aed69b7841611695c6c743c31892d8
luiz furtado <luizfurtadomkt@hotmail.com>
2021-09-24 14:41:16 -0300

    resolvendo erro de lint

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function RecipeCard({ id, name, path, thumb }) {
   return (
     <div data-testid={ `${id}-recipe-card` } className="item-card">
-      <Link to={ path } >
+      <Link to={ path }>
         <h2 data-testid={ `${id}-card-name` }>{ name }</h2>
         <img src={ thumb } alt={ name } data-testid={ `${id}-card-img` } />
       </Link>
